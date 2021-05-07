0bf5b899704617a425402679fbc6a84d02e6bf33
unknown <you@example.com>
2021-05-07 04:04:05 -0700

    Second commit

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express')
 
 const app = express()
 
-const PORT = 5000
+if(process.env.NODE_ENV === 'production') {
+
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
