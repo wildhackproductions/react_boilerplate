1530f02a161b5e2cd7a0969fd2bdd668a586f460
unknown <you@example.com>
2021-05-09 22:17:06 -0700

    Port number fixed

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ if(process.env.NODE_ENV === 'production') {
   })
 }
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
